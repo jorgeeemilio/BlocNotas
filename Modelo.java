987faa1b944b81393e9b7c911f2f6aad8b50343f
Jorge Rodríguez <jorge@grupostudium.com>
2021-04-15 09:32:31 +0200

    FileDialog

--- Modelo.java
+++ Modelo.java
@@ -1,6 +1,78 @@
 package es.studium.BlocNotas;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Modelo
 {
-
+	public int contarPalabras(String cadena)
+	{
+		int palabras = 0;
+		for(int i = 0; i<cadena.length(); i++)
+		{
+			if((cadena.charAt(i)==' ')||(cadena.charAt(i)=='\n'))
+			{
+				palabras++;
+			}
+		}
+		palabras++;
+		return palabras;
+	}
+	public int contarLetras(String cadena)
+	{
+		int letras = 0;
+		Pattern patron;
+		Matcher matcher;
+		patron = Pattern.compile("[a-zA-ZáéíóúÁÉÍÓÚ]");
+		for(int i = 0; i<cadena.length(); i++)
+		{
+			matcher = patron.matcher(cadena.charAt(i)+"");
+			if(matcher.matches())
+			{
+				letras++;
+			}
+		}
+		return letras;
+	}
+	public int contarVocales(String cadena)
+	{
+		int vocales = 0;
+		Pattern patron;
+		Matcher matcher;
+		patron = Pattern.compile("[aeiouAEIOUáéíóúÁÉÍÓÚ]");
+		for(int i = 0; i<cadena.length(); i++)
+		{
+			matcher = patron.matcher(cadena.charAt(i)+"");
+			if(matcher.matches())
+			{
+				vocales++;
+			}
+		}
+		return vocales;
+	}
+	public void guardarFichero(String ruta, String cadena)
+	{
+		//FileWriter también puede lanzar una excepción
+		try
+		{
+			// Destino de los datos
+			FileWriter fw = new FileWriter(ruta);
+			// Buffer de escritura
+			BufferedWriter bw = new BufferedWriter(fw);
+			// Objeto para la escritura
+			PrintWriter salida = new PrintWriter(bw);
+			//Guardamos la primera línea
+			salida.println(cadena);
+			//Cerrar el objeto salida, el objeto bw y el fw
+			salida.close();
+			bw.close();
+			fw.close();
+		}
+		catch(IOException i)
+		{}
+	}
 }
